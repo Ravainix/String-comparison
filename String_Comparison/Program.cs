ce38b0a1728d248820612313bdd520f62874203e
Lukasz <ravainix@gmail.com>
2019-03-18 23:11:47 +0100

    Some csv reading improvements

--- String_Comparison/Program.cs
+++ String_Comparison/Program.cs
@@ -7,51 +7,43 @@ namespace String_Comparison
 {
     class Program
     {
+        private const string Path = @"C:\Users\PC1\Desktop\String\sample2.csv";
+
         static void Main(string[] args)
         {
             Comparer comparer = new Comparer();
 
-
-
-            //Console.WriteLine(comparer.LevenshteinDistance("123", "132"));
-            //Console.WriteLine(comparer.Similarity("123", "122"));
-
-            using (var reader = new StreamReader("/Users/lukasz/Desktop/Context/Files/sample22.csv"))
-                //{
-                //    while (!reader.EndOfStream)
-                //    {
-                //        var line = reader.ReadLine();
-                //        Console.WriteLine(line);
-                //    }
-                //}
-
-
-                //using (var reader = new StreamReader("sample.csv"))
+            using (StreamReader reader = new StreamReader(Path))
             using (var csv = new CsvReader(reader))
             {
                
                csv.Configuration.Delimiter = ";";
                var records = new List<Foo>();
-
-                //csv.ReadHeader();
+                csv.Read();
+                csv.ReadHeader();
                 //Console.WriteLine(csv.Read());
                 //Console.WriteLine(csv.ReadHeader());
                 while (csv.Read())
                 {
-                    Console.WriteLine("Proccessing...");
-                    var record = new Foo
-                    {
-                        string1 = csv.GetField(1),
-                        string2 = csv.GetField(2)
-                    };
-                    records.Add(record);
+                    var string1 = csv.GetField(0);
+                    var distance = comparer.LevenshteinDistance(csv.GetField(1), csv.GetField(2));
+                    var similarity = comparer.Similarity(csv.GetField(1), csv.GetField(2));
+
+                    Console.WriteLine(String.Format("Proccessing {0}; {1}; {2}%", string1, distance, similarity*100));
+                    //var record = new Foo
+                    //{
+                    //    string1 = csv.GetField(1),
+                    //    string2 = csv.GetField(2)
+                    //};
+                    //records.Add(record);
                 }
 
                 //var records = csv.GetRecords<Foo>();
 
-                Console.WriteLine(records.Count);
+               // Console.WriteLine(records[0].string1);
             }
 
+            Console.ReadKey();
 
         }
     }
