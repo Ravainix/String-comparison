471582ce22f73c5774c9adf29ab85d38f2c2a400
Ravainix <ravainix@gmail.com>
2019-03-19 14:11:43 +0100

    Code improvements

--- String_Comparison/Comparer.cs
+++ String_Comparison/Comparer.cs
@@ -3,20 +3,15 @@ namespace String_Comparison
 {
     public class Comparer
     {
-        public Comparer()
-        {
-        }
 
-       public int LevenshteinDistance(string source, string target)
+        public int LevenshteinDistance(string source, string target)
         {
             if ((source == null) || (target == null)) return 0;
-            if ((source.Length == 0) || (target.Length == 0)) return 0;
-            if (source == target) return source.Length;
+            if (source == target) return 0;
 
             int sourceWordCount = source.Length;
             int targetWordCount = target.Length;
 
-            // Step 1
             if (sourceWordCount == 0)
                 return targetWordCount;
 
@@ -25,18 +20,20 @@ namespace String_Comparison
 
             int[,] distance = new int[sourceWordCount + 1, targetWordCount + 1];
 
-            // Step 2
-            for (int i = 0; i <= sourceWordCount; distance[i, 0] = i++) ;
-            for (int j = 0; j <= targetWordCount; distance[0, j] = j++) ;
+            for (int i = 0; i <= sourceWordCount; distance[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= targetWordCount; distance[0, j] = j++)
+            {
+            }
 
             for (int i = 1; i <= sourceWordCount; i++)
             {
                 for (int j = 1; j <= targetWordCount; j++)
                 {
-                    // Step 3
                     int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
 
-                    // Step 4
                     distance[i, j] = Math.Min(Math.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1), distance[i - 1, j - 1] + cost);
                 }
             }
