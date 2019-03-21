6a7a8e048ac57691f08c87785e1f01a9c7aa7289
Ravainix <ravainix@gmail.com>
2019-03-21 16:23:27 +0100

    Added new functionalities

--- String_Comparison/FileLoader.cs
+++ String_Comparison/FileLoader.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
 using System.IO;
+using System.Linq;
 using CsvHelper;
 
 namespace String_Comparison
@@ -11,6 +14,10 @@ namespace String_Comparison
         private Comparer comparer;
 
         public List<Record> records = new List<Record>();
+
+        public dynamic records2;
+
+        public string[] headers;
 
         public FileLoader(string path, Comparer comparer)
         {
@@ -31,6 +38,7 @@ namespace String_Comparison
                     csv.Configuration.Delimiter = delimiter;
                     csv.Read();
                     csv.ReadHeader();
+                    Console.WriteLine(csv.Context.HeaderRecord[1]);
                     Console.WriteLine("Starting processing...");
                     while (csv.Read())
                     {
@@ -44,7 +52,7 @@ namespace String_Comparison
                                 distance = comparer.LevenshteinDistance(csv.GetField(positionString1), csv.GetField(positionString2)),
                                 similarity = comparer.Similarity(csv.GetField(positionString1), csv.GetField(positionString2))
                             };
-
+                                
                             records.Add(record);
                         } catch 
                         {
@@ -63,17 +71,86 @@ namespace String_Comparison
             Console.WriteLine("Successfully processed " + records.Count + " records...");
         }
 
-
-        public void saveToFile(string filename)
+        public void loadCsv2(string delimiter)
         {
-            var savePath = Path.Combine(Directory.GetCurrentDirectory, filename);
-            using (var writer = new StreamWriter(savePath))
+            Console.WriteLine("Starting reading csv...");
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                using (var csv = new CsvReader(reader))
+                {
+                    Console.WriteLine("CSV found...");
+
+                    csv.Configuration.Delimiter = delimiter;
+                    csv.Read();
+                    csv.ReadHeader();
+                    headers = csv.Context.HeaderRecord;
+
+                   
+                    Console.WriteLine();
+
+                    Console.WriteLine("Headers loaded...");
+
+                    records2 = csv.GetRecords<dynamic>().ToList();
+                        //Console.WriteLine(String.Format("Proccessing {0}; Distance: {1}; Similarity: {2}", string1, distance, similarity));
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Cannot find file...");
+                //Console.WriteLine(a);
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Successfully loaded " + records2.Count + " records...");
+        }
+
+        public void proccess()
+        {
+
+            for (var i = 0; i < headers.Length; ++i)
+            {
+                Console.Write(i + "." + headers[i] + "   ");
+            }
+
+            //foreach (var item in records2)
+            //{
+            //    IDictionary<string, object> propertyValues = item;
+
+            //    foreach (var property in propertyValues.Keys)
+            //    {
+            //        Console.WriteLine(String.Format("{0} : {1}", property, propertyValues[property]));
+            //        Console.WriteLine();
+            //    }
+            //}
+
+            //foreach(var obj in records2)
+            //{
+            //    var el = (IDictionary<string, object>)obj;
+
+            //    Console.WriteLine(el["TABLE.OWN_PROD_NAME"]);
+            //}
+
+            //Console.WriteLine(records2[0].getProperty("TABLE.OWN_PROD_NAME"));
+            Console.WriteLine(records2[0].OWN_PN);
+
+
+        }
+
+
+        public void saveToFile()
+        {
+
+            var pathSave = Path.Combine(Directory.GetCurrentDirectory(), "output.csv");
+            
+                //filename = Path.Combine(Directory.GetCurrentDirectory(), filename + ".csv");
+
+            using (var writer = new StreamWriter(pathSave))
             using (var csv = new CsvWriter(writer))
             {
                 csv.WriteRecords(records);
             }
 
-            Console.WriteLine("Exported to " + savePath);
+            Console.WriteLine("Exported to " + pathSave);
         }
     }
 
